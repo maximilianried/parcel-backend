2e54fa597800e925a1d8afa9411b22d3d7d4bd6a
maximilianried <79225941+maximilianried@users.noreply.github.com>
2022-05-31 01:39:55 +0200

    Test with data tables for save and get

--- src/test/groovy/com/maximilianried/parcelbackend/ParcelBackendApplicationTests.groovy
+++ src/test/groovy/com/maximilianried/parcelbackend/ParcelBackendApplicationTests.groovy
@@ -1,13 +1,49 @@
 package com.maximilianried.parcelbackend
 
-import org.junit.jupiter.api.Test
+import com.maximilianried.parcelbackend.controller.ParcelController
+import com.maximilianried.parcelbackend.model.Parcel
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import spock.lang.Specification
 
-@SpringBootTest
-class ParcelBackendApplicationTests {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class ParcelBackendApplicationTests extends Specification {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private ParcelController parcelController
+
+	TestRestTemplate restTemplate = new TestRestTemplate()
+
+	def "save and get parcels"() {
+
+		// Create the parcel and save it
+		Parcel parcel = new Parcel()
+		parcel.setSenderName(senderName)
+		parcel.setSenderAddress(senderAddress)
+		parcel.setReceiverName(receiverName)
+		parcel.setReceiverAddress(receiverAddress)
+		parcelController.saveParcel(parcel)
+
+		// Get the parcel and check the response
+		expect:
+		ResponseEntity<Parcel> responseEntity = restTemplate.getForEntity("http://localhost:8080/parcel/get/" + index, Parcel)
+		responseEntity.statusCode == HttpStatus.OK
+		responseEntity.getBody().senderName == senderName
+		responseEntity.getBody().senderAddress == senderAddress
+		responseEntity.getBody().receiverName == receiverName
+		responseEntity.getBody().receiverAddress == receiverAddress
+		responseEntity.getBody().status == statusCode
+
+		// Data table with the values for the parcels
+		where:
+		index 	| senderName 	| senderAddress 	| receiverName 	| receiverAddress 	| statusCode
+		1 		| "Max" 		| "Musterstraße" 	| "Paul" 		| "Grimmstraße" 	| 1
+		2 		| "Jake" 		| "Westminster" 	| "Tom" 		| "Bonnstraße" 		| 1
+		3 		| "Tim" 		| "Tomstreet" 		| "Tik" 		| "Koelnstraße" 	| 1
+		4 		| "Peter" 		| "Anystreet" 		| "Track" 		| "Verteilerstraße" | 1
 	}
 
 }
