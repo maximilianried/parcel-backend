d23053e250ff236740647eceed3f5bc147334c01
maximilianried <79225941+maximilianried@users.noreply.github.com>
2022-05-30 19:43:23 +0200

    Created the parcel controller

---
+++ src/main/groovy/com/maximilianried/parcelbackend/controller/ParcelController.groovy
@@ -0,0 +1,62 @@
+package com.maximilianried.parcelbackend.controller
+
+import com.maximilianried.parcelbackend.model.Parcel
+import com.maximilianried.parcelbackend.service.ParcelService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/parcel")
+class ParcelController {
+
+    @Autowired
+    private ParcelService parcelService
+
+    // Append date and status-code then save parcel
+    @PostMapping("/save")
+    def saveParcel(@RequestBody Parcel parcel) {
+        parcel.date = new Date()
+        parcel.status = 1
+        parcelService.saveParcel(parcel)
+        return "Parcel is saved"
+    }
+
+    // Get all parcels as list
+    @GetMapping("/getAll")
+    def getAllParcels() {
+        return parcelService.getAllParcels()
+    }
+
+    // Get parcel by id
+    @GetMapping("/get/{id}")
+    def getParcel(@PathVariable (value = "id") long id) {
+        return parcelService.getParcel(id)
+    }
+
+    // Delete parcel by id
+    @DeleteMapping("/delete/{id}")
+    def deleteParcel(@PathVariable (value = "id") long id) {
+        parcelService.deleteParcel(id)
+        return "Parcel with id ${id} is deleted"
+    }
+
+    @DeleteMapping("/deleteAll")
+    def deleteAllParcels() {
+        parcelService.deleteAllParcels()
+        return "All parcels are deleted"
+    }
+
+    // Update parcel status by id and status-code
+    @PutMapping("/update/status/{id}/{status}")
+    def changeStatus(@PathVariable (value = "id") long id, @PathVariable (value = "status") int status) {
+        parcelService.changeStatus(id, status)
+        return "Parcel with id ${id} changed status to ${status}"
+    }
+}
