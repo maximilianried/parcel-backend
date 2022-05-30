58e91d08eb013d3a9c50a2763f74e619d9dd8be6
Maximilian Ried <79225941+maximilianried@users.noreply.github.com>
2022-05-30 18:20:55 +0200

    Initial commit

---
+++ src/main/groovy/com/maximilianried/parcelbackend/ParcelBackendApplication.groovy
@@ -0,0 +1,13 @@
+package com.maximilianried.parcelbackend
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class ParcelBackendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ParcelBackendApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/maximilianried/parcelbackend/ParcelBackendApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.maximilianried.parcelbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ParcelBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
